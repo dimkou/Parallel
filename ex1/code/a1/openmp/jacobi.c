53b62fa95849e3a2e1f0f70c46d741e007e7eab2
VHarisop <the.machine.headed@gmail.com>
2014-12-12 22:47:37 +0200

    Complete basic Jacobi, Makefile update

--- ex1/code/a1/openmp/jacobi.c
+++ ex1/code/a1/openmp/jacobi.c
@@ -3,6 +3,17 @@
 #include <omp.h>
 #include "utils.h"
 
+void jacobi(double ** u_previous, double ** u_current, int min_x, int max_x, int min_y, int max_y) 
+{
+	// computational core here
+	// called from within threads
+	int i, j;
+	for (i = min_x; i < max_x; i++)
+		for (j = min_y; j < max_y; j++)
+			u_current[i][j] = (u_previous[i-1][j] + u_previous[i][j-1] + u_previous[i+1][j] + u_previous[i][j+1]) / 4.0;
+}
+
+
 int main(int argc, char ** argv)
 {
 	int global[2], local[2];  // global/local matrix dim
@@ -10,12 +21,10 @@ int main(int argc, char ** argv)
 
 	int grid[2];	      	// processors per dimension
 
-	struct timeval tts, ttf, tcs, tcf; // total and computation timers
+	// struct timeval tts, ttf, tcs, tcf; // total and computation timers(TODO: use them!)
 
-	double **U; 
 	double ** u_current, ** u_previous; // matrices for iterations
 
-	/* TODO: Code is to be filled here */
 
 	
 	if (argc != 5)
@@ -46,32 +55,66 @@ int main(int argc, char ** argv)
         }
     }
 
-	// allocate global matrix, initialize random values on boundary
+	// allocate initial matrix, initialize random values on boundary
 	// and zero values on interior
-	U = allocate2d(global_padded[0], global_padded[1]);
-	init2d(U, global[0], global[1]); //possible bug? (sizes differ)
+	u_previous = allocate2d(global_padded[0], global_padded[1]);
+	init2d(u_previous, global[0], global[1]); //possible bug? (sizes differ)
 
-	// allocate u_current and u_previous
-
-	printf("Global Padded: %d %d\n", global_padded[0], global_padded[1]);
-	u_previous = U;
+	// allocate u_current
 	u_current = allocate2d(global_padded[0], global_padded[1]);
 
-	omp_set_num_threads(grid[0] * grid[1]);
+	// transfer boundary values
+	copy2d(u_current, u_previous, global[0], global[1]);
+
+	// set number of threads
 	int nthreads = grid[0] * grid[1];
-	#pragma omp parallel 
+	omp_set_num_threads(nthreads);
+
+	// convergence flags
+	int converged = 0;
+	int globalConv = 0;
+
+	#pragma omp parallel
 	{
 		// calculate block limits
 		int tid = omp_get_thread_num();
-		int i, j; 
+		int i, j, endi, endj, conv;
 
 		i = (global_padded[0] / grid[0]) * (tid % grid[0]);
 		j = (global_padded[1] / grid[1]) * (tid / grid[0]);
 
-		#ifdef DEBUG
-			printf("Thread %d/%d gets (%d, %d) indices\n", tid, nthreads, i, j);
-		#endif
-	}
+		// calculate index limits. Locations on the edge of the matrix are not calculated
+		endi = ((i + local[0]) < global[0]) ? i + local[0] : (global[0] - 1);
+		endj = ((j + local[1]) < global[1]) ? j + local[1] : (global[1] - 1);
+		
+		// fix start limits for edges of matrix
+		i += (i == 0);
+		j += (j == 0);
 
+		#ifdef DEBUG
+			printf("Thread %d/%d gets (%d-%d, %d-%d) indices\n", tid, nthreads, i, endi, j, endj);
+		#endif
+		
+				
+		while (!globalConv) {
+			jacobi(u_previous, u_current, i, endi, j, endj);
+			conv = converge(u_previous, u_current, i, endi, j, endj);
+			
+			#pragma omp atomic
+			converged += conv;
+
+			#pragma omp barrier
+			if (tid == 0) {
+				if (converged == nthreads) { globalConv = 1; }
+				else { converged = 0; }
+				if (! globalConv) { copy2d(u_previous, u_current, global[0], global[1]); }
+			}
+			
+			#pragma omp barrier
+	
+		}
+
+	}
+	fprint2d("test.out", u_current, global[0], global[1]);
 	return 0;
 }
